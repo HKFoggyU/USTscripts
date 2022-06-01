9400c96cd23321825c249647d900b52305c802c5
github-young <xymailmooc@163.com>
2022-06-01 20:40:07 +0800

    update PLP: add item count

--- scripts/漂流瓶价格.user.js
+++ scripts/漂流瓶价格.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         漂流瓶价格
 // @namespace    https://hkfoggyu.github.io/
-// @version      0.5
+// @version      0.6
 // @description  显示漂流瓶每个瓶子的总重和价格(单件物品的瓶子无法显示)
 // @author       Young
 // @supportURL   https://github.com/HKFoggyU/USTscripts
-// @updateURL    https://github.com/HKFoggyU/USTscripts/raw/main/%E6%BC%82%E6%B5%81%E7%93%B6%E4%BB%B7%E6%A0%BC.user.js
-// @downloadURL  https://github.com/HKFoggyU/USTscripts/raw/main/%E6%BC%82%E6%B5%81%E7%93%B6%E4%BB%B7%E6%A0%BC.user.js
 // @match        http://www.piaoliuhk.com/packageList.php
 // @icon         http://www.piaoliuhk.com/css/images/favicon.ico
 // @grant        none
-// $run-at       document-end
+// @license      MIT
+// @run-at       document-end
 // ==/UserScript==
 
 function getBottles() {
@@ -18,7 +17,6 @@ function getBottles() {
     var bottles = [];
     for (var i=1; i<tds.length; i++) {
         if (tds[i].hasAttribute("rowspan")) {
-            //console.log(`Bottle Num: ${tds[1].innerText}`);
             bottles.push(tds[i]);
         }
     }
@@ -27,17 +25,23 @@ function getBottles() {
 
 function calcWeightAndPrice(bottleNum) {
     var total = 0.0;
+    let updatedNumber = 0;
+    let addedNumber = 0;
     var trs = document.getElementsByTagName("tr");
     for (var i=1; i<trs.length; i++) {
         var tds = trs[i].getElementsByTagName("td");
-        if (tds[7].innerText != "" && tds[1].innerText == bottleNum) {
-            total += parseFloat(tds[7].innerText);
+        if (tds[1].innerText == bottleNum) {
+            addedNumber += 1;
+            if (tds[7].innerText != "") {
+                total += parseFloat(tds[7].innerText);
+                updatedNumber += 1;
+            }
         }
     }
     var totalWeight = total.toFixed(1);
     var totalPriceHKD = totalWeight>1.0 ? (Math.ceil((totalWeight-1)/0.5)*3.5+12.5).toFixed(1) : 12.5;
     var totalPriceCNY = totalWeight>1.0 ? (Math.ceil((totalWeight-1)/0.5)*3.2+11.4).toFixed(1) : 11.4;
-    return [totalWeight, totalPriceHKD, totalPriceCNY];
+    return [updatedNumber, addedNumber, totalWeight, totalPriceHKD, totalPriceCNY];
 }
 
 (function() {
@@ -46,14 +50,10 @@ function calcWeightAndPrice(bottleNum) {
     for (var i=0; i<bottles.length; i++) {
         var bottle = bottles[i];
         var bottleNum = bottle.innerText;
-        var [totalWeight, totalPriceHKD, totalPriceCNY] = calcWeightAndPrice(bottleNum);
-        var outputText = `${bottle.innerText} (${totalWeight} kg, ${totalPriceHKD} HKD, ${totalPriceCNY} CNY)`;
-        //console.log(outputText);
+        var [updatedNumber, addedNumber, totalWeight, totalPriceHKD, totalPriceCNY] = calcWeightAndPrice(bottleNum);
+        var outputText = `${bottle.innerText} (${updatedNumber}/${addedNumber} items, ${totalWeight} kg, ${totalPriceHKD} HKD, ${totalPriceCNY} CNY)`;
         bottle.innerText=outputText;
-
-        //var outputStr = `漂流瓶编号: ${bottleNum}\n总重: ${totalWeight} kg.\n总价: ${totalPriceHKD} HKD.`;
-        //console.log(outputStr);
-        //window.alert(outputStr);
     }
     //window.scrollTo(0,document.body.scrollHeight);
 })();
+
